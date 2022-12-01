030f6b7d9634e9a2edfd888cf636dfbe1a57716c
superzilong <zilong919@163.com>
2022-12-01 23:06:05 +0800

    First submit

---
+++ leetCode1334.h
@@ -0,0 +1,15 @@
+// leetCode 1334 Find the city With the Smallest Number of Neighbors at a Threshold Distance EP303
+// https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/submissions/
+
+#include <vector>
+
+class Solution
+{
+public:
+    int findTheCity(int n, std::vector<std::vector<int>>& edges, int distanceThreshold)
+    {
+        std::vector<std::vector<int>> dp(n, std::vector<int>(n, INT_MAX/2));
+
+    }
+
+};
